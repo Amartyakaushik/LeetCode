bf7977ff51ba823dd6cfca8f43783bbf19863d6b
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2025-02-23 23:17:41 +0530

    Time: 6 ms (10.24%), Space: 9.4 MB (27.85%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -2,24 +2,33 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        map<char, char> m;
-        set<char> st;
-        for (int i = 0; i < t.length(); i++) {
-            char tt = t[i];
-            char ss = s[i];
-
-            if (m.find(tt) != m.end()) {
-                if (m[tt] != ss) {
-                    return false;
-                }
-            } else {
-                if (st.find(ss) != st.end()) {
-                    return false;
-                }
-                m[tt] = ss;
-                st.insert(ss);
-            }
+        map<char, char> ms, mt;
+        // for (int i = 0; i < s.size(); i++) {
+        //     m[s[i]] = t[i];
+        // }
+        for (int i = 0; i < s.size(); i++) {
+            if((ms.count(s[i])) && (ms[s[i]] != t[i])) return false;
+            if((mt.count(t[i])) && (mt[t[i]] != s[i])) return false;
+            ms[s[i]] = t[i];
+            mt[t[i]] = s[i];
         }
+        // set<char> st;
+        // for (int i = 0; i < t.length(); i++) {
+        //     char tt = t[i];
+        //     char ss = s[i];
+
+        //     if (m.find(tt) != m.end()) {
+        //         if (m[tt] != ss) {
+        //             return false;
+        //         }
+        //     } else {
+        //         if (st.find(ss) != st.end()) {
+        //             return false;
+        //         }
+        //         m[tt] = ss;
+        //         st.insert(ss);
+        //     }
+        // }
         return true;
     }
 };
