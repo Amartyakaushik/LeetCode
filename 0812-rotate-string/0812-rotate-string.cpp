4bb8c978800f2ed60da20cb8752196ee40baaf4a
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2025-02-25 01:07:14 +0530

    Time: 0 ms (100%), Space: 8.4 MB (25.06%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -1,6 +1,40 @@
 class Solution {
 public:
+
+    vector<int> computeLPS(string goal){
+        int n = goal.size();
+        vector<int> pi(n, 0);
+        for(int i = 1; i < n; i++){
+            int j = pi[i - 1];
+            while(goal[i] != goal[j] && j > 0){
+                j = pi[j -1];
+            }
+            if(goal[i] == goal[j]){
+                j++;
+            }
+            pi[i] = j;
+        }
+        return pi;
+    }
+
+    bool KMP(string s, string goal){
+        int n = s.size(), m = goal.size();
+        vector<int> LPS = computeLPS(goal);
+
+        int i = 0, j = 0;
+        while(i < n){
+            if(s[i] == goal[j]){
+                i++, j++;
+                if(j == m) return true;
+            }else{
+                if(j) j = LPS[j -1];
+                else i++;
+            }
+        }
+        return false;
+    }
     bool rotateString(string s, string goal) {
+        
         // // naive approach 
         // for(int i = 0; i < s.size(); i++){
         //     if(s == goal) return true;
@@ -10,8 +44,13 @@ public:
         // }
         // return false;
 
-        // alternate approach
+        // // alternate approach
+        // if(s.size() != goal.size()) return false;
+        // return (s + s).find(goal) != string::npos;
+
+        // using KMP
         if(s.size() != goal.size()) return false;
-        return (s + s).find(goal) != string::npos;
+        return KMP(s + s , goal);
+
     }
 };
