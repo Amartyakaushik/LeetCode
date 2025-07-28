20a412122f619bcb297ec1ff70b1e3b9ea6098f0
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2025-07-28 23:22:23 +0530

    Time: 1 ms (61.1%), Space: 9.8 MB (26.46%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,23 +1,18 @@
-#include<bits/stdc++.h>
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        // map<char, int> checkCount;
-        // for(char c : s){
-        //     checkCount[c]++;
-        // }
-        // for(char c : t){
-        //     checkCount[c]--;
-        // }
-        // for(const auto& i : checkCount){
-        //     if(i.second > 0) return false;
-        // }
-        // if(s.size() != t.size()) return false;
-        // return true;
-
-        // alternative approach
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return (s == t);
+        if(s.size() != t.size()) return false;
+        unordered_map<char, int> checkS;
+        unordered_map<char, int> checkT;
+        for(char c : s){
+            checkS[c]++;
+        }
+        for(char c : t){
+            checkT[c]++;
+        }
+        for(char c : t){
+            if(checkS[c] != checkT[c]) return false;
+        }
+        return true;
     }
 };
