3f666b83c7f99426c8fa8280f0ae63f514625fc0
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2025-08-07 23:10:49 +0530

    Time: 3 ms (94.55%), Space: 22.9 MB (73.51%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-    bool check(vector<int>& piles, long mid, int speed) {
-
+    bool check(vector<int>& piles, long mid, int speed){
         long time = 0;
-        for (int i = 0; i < piles.size(); i++) {
-            time += piles[i] / mid;
-            if (piles[i] % mid != 0)
-                time++;
+        for(int i : piles){
+            time += (i / mid);
+            if((i % mid) != 0) time++;
         }
         return (time <= speed);
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        // using binary search
         int high = *max_element(piles.begin(), piles.end());
-        int low = 1, res = high;
-        while (low <= high) {
+        int ans = high, low = 1;
+        while(low <= high){
             int mid = (low + high) / 2;
-            if (check(piles, mid, h)) {
-                high = mid - 1;
-                res = mid;
-            } else {
+            if(check(piles, mid, h)){
+                high = mid -1;
+                ans = mid;
+            }else{
                 low = mid + 1;
             }
         }
-        return res;
+        return ans;
     }
 };
