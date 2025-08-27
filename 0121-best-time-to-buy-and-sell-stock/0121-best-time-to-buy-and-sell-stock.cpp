7bdb0b0ba8366048c315df72fcf6e41ec696c932
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2025-08-27 22:45:50 +0530

    Time: 1 ms (36.61%), Space: 97.3 MB (86.93%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,12 +1,24 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int minBuyingPrice = INT_MAX, maxProfit = 0;
-        for (int i = 0; i < prices.size(); i++) {
-            maxProfit = max(maxProfit, prices[i] - minBuyingPrice);
-            if (minBuyingPrice > prices[i])
-                minBuyingPrice = prices[i];
+        // unordered_map<int, int> maxProfit;
+        // int maxP = 0;
+        // int startIx = 0;
+        // while(startIx < prices.size() - 1){
+        //     for(int i = startIx + 1; i < prices.size(); i++){
+        //     // maxProfit[prices[i]] = max(maxProfit[prices[i]], (prices[startIx] - prices[i]));
+        //     maxP = max(maxP, (prices[i] - prices[startIx]));
+        //     }
+        //     startIx++;
+        // }
+        // return maxP;
+        int minBuyingPrice = INT_MAX;
+        int maxProfit = 0;
+        for(int i = 0; i < prices.size(); i++){
+            maxProfit = max(maxProfit, (prices[i] - minBuyingPrice));
+            if(minBuyingPrice > prices[i]) minBuyingPrice = prices[i];
         }
         return maxProfit;
+
     }
 };
