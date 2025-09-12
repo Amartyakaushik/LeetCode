61f70b15bc7ce65617565b62955bcbec82f547c4
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2025-09-12 23:01:56 +0530

    Time: 33 ms (18.29%), Space: 27.1 MB (17.54%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,34 +1,26 @@
 class Solution {
-public:
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> targetCombinations;
-        // for(int x = 0; x < candidates.size(); x++){
-        //     // 
-        //     if(target % x == 0){
-        //         vector<int> combination;
-        //         for(int i = 0; i < (target / x); i++){
-        //             combination.push_back(x);
-        //         }
-        //         targetCombinations.push_back(combination);
-        //     }
-            
-        // }
-
-        sort(candidates.begin(), candidates.end());
-        vector<int> currComb;
-        function<void(int, int)> dfs = [&](int ix, int remaining) {
+    private:
+        void check(vector<int> candidates, int remaining, vector<int> currPath, int index, vector<vector<int>>& ans){
             if(remaining == 0){
-                targetCombinations.emplace_back(currComb);
+                ans.emplace_back(currPath);
                 return;
             }
-            if(ix >= candidates.size() || remaining < candidates[ix])
-                return;
-            dfs(ix + 1, remaining);
-            currComb.push_back(candidates[ix]);
-            dfs(ix, remaining - candidates[ix]);
-            currComb.pop_back();
-        };
-        dfs(0, target);
-        return targetCombinations;
+            if(index >= candidates.size() || remaining < candidates[index]) return;
+
+            // exclude
+            check(candidates, remaining, currPath, index+1, ans);
+            currPath.push_back(candidates[index]);
+            check(candidates, remaining - candidates[index], currPath, index, ans);
+            currPath.pop_back();
+            
+        }
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        sort(candidates.begin(), candidates.end());
+        vector<int> currPath;
+        check(candidates, target, currPath, 0, ans);
+        return ans;
+
     }
 };
