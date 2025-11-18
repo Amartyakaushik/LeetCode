4a7a119239b85a49cb9f994addc9e9071c7b2b3d
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2025-11-18 20:42:29 +0530

    Time: 10 ms (30.69%), Space: 10.9 MB (69.52%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,37 +1,17 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        // int maxLen = 1, maxS = 1, changeCount = k, startix = 0, count = 0;
-        // for(int i = 1; i < s.size(); i++){
-        //     if(s[i] == s[startix]){
-        //         maxLen++;
-        //     }else{
-        //         if(count == 0){
-        //             startix = i, count++;
-        //         }
-        //         if((--changeCount) >= 0){
-        //             maxLen++;
-        //         }else{
-        //             maxS = max(maxLen, maxS);
-        //             i = startix, maxLen = 1, count = 0, changeCount = k;
-        //             // break;
-        //         }
-                
-        //     }
-        //     maxS = max(maxLen, maxS);
-        // }
-
-        unordered_map<char, int> count;
-        int maxCharCount = 0, left = 0, r = 0;
-        for(r = 0; r < s.size(); r++){
-            count[s[r]]++;
-            maxCharCount = max(maxCharCount, count[s[r]]);
-            // check if window size is exceeding the lenght according to the conditions
-            if((r - left + 1 ) - maxCharCount > k){
-                count[s[left]]--;
+        int maxFreq = 0, left = 0, ans = 0;
+        unordered_map<char, int> freq;
+        for(int r = 0; r < s.size(); r++){
+            freq[s[r]]++;
+            maxFreq = max(maxFreq, freq[s[r]]);
+            while((r - left + 1) - maxFreq > k){
+                freq[s[left]]--;
                 left++;
             }
+            ans = max(ans, (r - left + 1));
         }
-        return r - left;
+        return ans;
     }
 };
